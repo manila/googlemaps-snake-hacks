447fff0c182027dc5f959840279f5cb37ff5d6ec
manila <manila@users.noreply.github.com>
2019-04-07 15:56:09 -0600

    Updated to support Snake V20.js

--- googlemaps-snake-bot-commented.js
+++ googlemaps-snake-bot-commented.js
@@ -1,14 +1,15 @@
 (function () {
+	let gameObj = R;
 	/* Keep track of positions as the board is checked more often than it is drawn */
 	/* The Q.b.o.b[0].b Object is where the snake data is stored, this is obsfucated by Google */
-	oldPosX = Q.b.o.b[0].b.x;
-	oldPosY = Q.b.o.b[0].b.y;
+	oldPosX = gameObj.b.o.b[0].b.x;
+	oldPosY = gameObj.b.o.b[0].b.y;
 
 	/* Move snake */
 	function moveSnake() {
 		/* Get current snake (head) position */
-		curPosX = Q.b.o.b[0].b.x;
-		curPosY = Q.b.o.b[0].b.y;
+		curPosX = gameObj.b.o.b[0].b.x;
+		curPosY = gameObj.b.o.b[0].b.y;
 
 		/* Check if snake has moved since we checked it's position last */
 		if (oldPosX !== curPosX || oldPosY !== curPosY)
@@ -24,29 +25,29 @@
 				if (curPosX % 2 == 0)
 				{
 					/* The Q.s() function is called by a keydown event listener, this is obsfucated by Google */
-					Q.s({key: "Up"});
+					gameObj.s({key: "Up"});
 					console.log("Going Up");
 				}
 				else
 				{
-					Q.s({key: "Down"});
+					gameObj.s({key: "Down"});
 					console.log("Going Down");
 				}
 
 
 				/* If snake gets to top or one up from the bottom of the grid turn right */
 				if ((curPosY == 18 && curPosX % 2 !== 0 && curPosX != 19) || (curPosY == 0 && curPosX % 2 == 0)) {
-					Q.s({key: "Right"});
+					gameObj.s({key: "Right"});
 					console.log("Turn Right");
 				}
 			}
 
 			/* This handles the snake navigation in the bottom row of the board */
 			if (curPosX == 19 && curPosY == 19) {
-				Q.s({key: "Left"});
+				gameObj.s({key: "Left"});
 				console.log("Turn Left");
 			} else if (curPosX == 0 && curPosY == 19) {
-				Q.s({key: "Up"});
+				gameObj.s({key: "Up"});
 				console.log("Turn Right");
 			}
 	
